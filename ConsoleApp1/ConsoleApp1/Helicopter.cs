ba3338b12af08d4a87d5ba8336c44000ee329cdf
AlbanoRavenPOGI <onlyven711@gmail.com>
2026-06-27 14:56:22 +0800

    Commit Final Done

--- ConsoleApp1/ConsoleApp1/Helicopter.cs
+++ ConsoleApp1/ConsoleApp1/Helicopter.cs
@@ -4,7 +4,7 @@ public class Helicopter : Vehicle, IFlyable, IDriveable
 {
     public Helicopter() : base("Helicopter") { }
 
-    public override string Move() => $"{Fly()} {Drive()}";
+    public override string Move() => Fly();
 
     public string Fly() => "Flying in the sky.";
     public string Drive() => "Driving on the road.";
